fb1b8fb55d7f54f21ba088474f754b8a66a59541
KIRA <125059234+MasoomaTahira@users.noreply.github.com>
2023-06-28 15:23:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let ampm = document.getElementById("ampm");
+
 
 setInterval(()=> {
 let currentTime = new Date();
 
-hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+ 
+    if(currentTime.getHours()<=12) {
+       hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    } 
+    else {
+    hrs.innerHTML = (currentTime.getHours()-12);
+    ampm.innerHTML = 'PM';
+    }
+    
 min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
 sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
 },1000)
